170d8db19a5aa652d3044996d5d7da774e762cf3
chenlong <951709483@qq.com>
2020-07-23 17:08:43 +0800

    Initial commit

---
+++ mall_user/src/main/java/com/cl/user/UserApplication.java
@@ -0,0 +1,17 @@
+package com.cl.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenlong
+ * @version 1.0
+ * @description 用户工程启动类
+ * @date
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class,args);
+    }
+}
